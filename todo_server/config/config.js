8726e2d6459bf883f7aa4cd71ca9d50c5d08c7a2
beacortonezi <beatrizcortonezi@gmail.com>
2019-12-03 15:40:48 -0400

    Inicializando

--- todo_server/config/config.js
+++ todo_server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   database_user: 'root',
-  database_password: '',
-  database_name: 'todo',
+  database_password: 'root1234',
+  database_name: 'task',
   jwtSecret: "MyS3cr3tK3Y",
   jwtSession: {session: false}
 }
